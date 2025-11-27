904c1d0b70c9519c451f04720e711bc2e8bcf471
akfaqih3 <88878961+akfaqih3@users.noreply.github.com>
2025-11-26 16:11:18 -0800

    Enable database configuration in settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -93,12 +93,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': config('DB_ENGINE'),
-#         'NAME': config('DB_NAME'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': config('DB_ENGINE', default='django.db.backends.sqlite3'),
+        'NAME': config('DB_NAME', default='db.sqlite3'),
+    }
+}
 
 
 # Password validation
